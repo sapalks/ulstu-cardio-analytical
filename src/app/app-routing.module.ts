97503fcef3040134016c1f91aec124be7ae437c9
Sapunkov Alexey <sapalks@gmail.com>
2022-07-09 17:27:55 +0400

    add components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { PnfComponent } from './pnf/pnf.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ListComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PnfComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { ListComponent } from './list/list.component';
+import { PnfComponent } from './pnf/pnf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent,
+    ListComponent,
+    PnfComponent
   ],
   imports: [
     BrowserModule,
