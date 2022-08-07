bd97c15ff358548fd7c9c7182fdc4916c9b9a929
Sapunkov Alexey <sapalks@gmail.com>
2022-08-07 12:59:01 +0400

    add all forms

--- src/app/additional/additional.component.ts
+++ src/app/additional/additional.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { debounceTime, Observable, Subject, takeUntil } from 'rxjs';
 import { ApiService } from '../api.service';
 import { FullStore, UserFullModel } from '../store/store.model';
 import { loading } from '../store/user.selector';
@@ -14,19 +15,75 @@ import { loading } from '../store/user.selector';
 export class AdditionalComponent implements OnInit {
 
   user$: Observable<UserFullModel | null>;
+  userId: string;
   loading$: Observable<boolean>;
+  private unsubscribe$: Subject<void> = new Subject();
+
+  form: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private api: ApiService,
-    private store: Store<FullStore>
+    private store: Store<FullStore>,
+    private readonly fb: FormBuilder,
   ) {
+    this.userId = this.route.snapshot.paramMap.get('userId')!;
     this.user$ = this.api.getUser();
     this.loading$ = this.store.select(loading);
+
+    this.form = this.fb.group({
+      highDensityCholesterol: [0, [Validators.required, Validators.min(0.5), Validators.max(5.5)]],
+      lowDensityCholesterol: [0, [Validators.required, Validators.min(0.5), Validators.max(8.5)]],
+      triglycerides: [0, [Validators.required, Validators.min(0.2), Validators.max(8.5)]],
+      lipoprotein: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      ecg: [undefined, [Validators.required,]],
+      protein: [0, [Validators.required, Validators.min(0.1), Validators.max(12)]],
+      atherogenicCoefficient: [0, [Validators.required, Validators.min(0.1), Validators.max(8)]],
+      creatinine: [0, [Validators.required, Validators.min(20), Validators.max(500)]],
+      havePlaques: [false, [Validators.required,]],
+    });
+
+    for (const key of Object.keys(this.form.controls)) {
+      this.form.controls[key].valueChanges.pipe(debounceTime(500))
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(value => {
+          this.api.update(this.userId, { [key]: value })
+        });
+    }
+
+    this.api.getUser().pipe(takeUntil(this.unsubscribe$)).subscribe(user => user && this.setForm(user));
+
   }
 
   ngOnInit(): void {
+    window.scrollTo(0, 0);
   }
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.unsubscribe();
+  }
+  setForm(user: UserFullModel) {
+    console.log(user)
+    const keys = Object.keys(this.form.controls);
+    for (const key of keys) {
+      this.form.controls[key].setValue((user as any)[key], {
+        emitEvent: false,
+        emitModelToViewChange: false,
+        emitViewToModelChange: false,
+      });
+    }
+  }
+
+  yn = [
+    {
+      name: 'Да',
+      value: true
+    },
+    {
+      name: 'Нет',
+      value: false
+    },
+  ]
 
 }
--- src/app/lifestyle/lifestyle.component.ts
+++ src/app/lifestyle/lifestyle.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { debounceTime, Observable, Subject, takeUntil } from 'rxjs';
 import { ApiService } from '../api.service';
 import { CommitmentEnum, FamilyStatusEnum, FullStore, InSocialEventsEnum, PhysicalActivityEnum, SexEnum, SymptomsOfAnginaPectorisEnum, UserFullModel, WorkStatusEnum } from '../store/store.model';
 import { loading } from '../store/user.selector';
@@ -14,95 +15,188 @@ import { loading } from '../store/user.selector';
 export class LifestyleComponent implements OnInit {
 
   user$: Observable<UserFullModel | null>;
+  userId: string;
   loading$: Observable<boolean>;
+  private unsubscribe$: Subject<void> = new Subject();
+
+  form: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private api: ApiService,
-    private store: Store<FullStore>
+    private store: Store<FullStore>,
+    private readonly fb: FormBuilder,
   ) {
+    this.userId = this.route.snapshot.paramMap.get('userId')!;
     this.user$ = this.api.getUser();
     this.loading$ = this.store.select(loading);
+
+    this.form = this.fb.group({
+      familyStatus: [undefined, Validators.required],
+      inSocialEvents: [undefined, Validators.required],
+      physicalActivity: [undefined, Validators.required],
+      workStatus: [undefined, Validators.required],
+      valuation1: [undefined, Validators.required],
+      valuation2: [undefined, Validators.required],
+      valuation3: [undefined, Validators.required],
+    });
+
+    for (const key of Object.keys(this.form.controls)) {
+      this.form.controls[key].valueChanges.pipe(debounceTime(500))
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(value => {
+          this.api.update(this.userId, { [key]: value })
+        });
+    }
+
+    this.api.getUser().pipe(takeUntil(this.unsubscribe$)).subscribe(user => user && this.setForm(user));
+
   }
 
   ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.unsubscribe();
+  }
+  next() {
+    this.router.navigate(['user', this.userId, 'additional'], {
+      queryParams: this.route.snapshot.queryParams
+    });
   }
 
-  fs(value: FamilyStatusEnum | undefined, sex: SexEnum): string {
-    switch (value) {
-      case FamilyStatusEnum.MARRIED:
-        return sex === SexEnum.MAN ? 'женат' : 'замужем';
-      case FamilyStatusEnum.DIVORCED:
-        return sex === SexEnum.MAN ? 'разведен' : 'разведена';
-      case FamilyStatusEnum.WIDOWER:
-        return sex === SexEnum.MAN ? 'вдовец' : 'дова';
-      default:
-        return 'не указано';
+  setForm(user: UserFullModel) {
+    console.log(user)
+    const keys = Object.keys(this.form.controls);
+    for (const key of keys) {
+      this.form.controls[key].setValue((user as any)[key], {
+        emitEvent: false,
+        emitModelToViewChange: false,
+        emitViewToModelChange: false,
+      });
     }
   }
 
-  se(value: InSocialEventsEnum | undefined): string {
-    switch (value) {
-      case InSocialEventsEnum.ONE:
-        return 'один раз в неделю';
-      case InSocialEventsEnum.MORE:
-        return 'Более одного раза в неделю';
-      default:
-        return 'не указано';
-    }
-  }
+  fsMan = [
+    {
+      name: 'Женат',
+      value: FamilyStatusEnum.MARRIED
+    },
+    {
+      name: 'Разведен',
+      value: FamilyStatusEnum.DIVORCED
+    },
+    {
+      name: 'Вдовец',
+      value: FamilyStatusEnum.WIDOWER
+    },
+  ];
 
-  pa(value: PhysicalActivityEnum | undefined): string {
-    switch (value) {
-      case PhysicalActivityEnum.ONE:
-        return 'тренировка один раз в неделю';
-      case PhysicalActivityEnum.MORE:
-        return 'Тренировка более одного раза в неделю';
-      case PhysicalActivityEnum.DAILY:
-        return 'повседневная';
-      default:
-        return 'не указано';
-    }
-  }
+  fsWoman = [
+    {
+      name: 'Замужем',
+      value: FamilyStatusEnum.MARRIED
+    },
+    {
+      name: 'Разведена',
+      value: FamilyStatusEnum.DIVORCED
+    },
+    {
+      name: 'Вдова',
+      value: FamilyStatusEnum.WIDOWER
+    },
+  ];
 
-  w(value: WorkStatusEnum | undefined): string {
-    switch (value) {
-      case WorkStatusEnum.WORKERD:
-        return 'Работает';
-      case WorkStatusEnum.RETIRED:
-        return 'на пенсии';
-      case WorkStatusEnum.UNEMPLOYED:
-        return 'безработный';
-      case WorkStatusEnum.LOOKING:
-        return 'в поиске работы';
-      default:
-        return 'не указано';
-    }
-  }
+  se = [
+    {
+      name: 'Один раз в неделю',
+      value: InSocialEventsEnum.ONE,
+    },
+    {
+      name: 'Более одного раза в неделю',
+      value: InSocialEventsEnum.MORE,
+    },
+  ];
 
-  sap(value: SymptomsOfAnginaPectorisEnum | undefined): string {
-    switch (value) {
-      case SymptomsOfAnginaPectorisEnum.GOTO_CARDIOLOGIST:
-        return 'необходимо записаться к кардиологу';
-      case SymptomsOfAnginaPectorisEnum.NO_SYMPTOMS:
-        return 'Нет симптомов';
-      default:
-        return 'не указано';
-    }
-  }
+  pa = [
+    {
+      name: 'Тренировка один раз в неделю',
+      value: PhysicalActivityEnum.ONE
+    },
+    {
+      name: 'Тренировка более одного раза в неделю',
+      value: PhysicalActivityEnum.MORE
+    },
+    {
+      name: 'Повседневная',
+      value: PhysicalActivityEnum.DAILY
+    },
+  ];
 
-  c(value: CommitmentEnum | undefined): string {
-    switch (value) {
-      case CommitmentEnum.LOW:
-        return 'Низкий';
-      case CommitmentEnum.MEDIUM:
-        return 'Средний';
-      case CommitmentEnum.HIGTH:
-        return 'Высокий';
-      default:
-        return 'не указано';
-    }
-  }
+  w = [
+    {
+      name: 'Работает',
+      value: WorkStatusEnum.WORKERD
+    },
+    {
+      name: 'На пенсии',
+      value: WorkStatusEnum.RETIRED
+    },
+    {
+      name: 'Безработный',
+      value: WorkStatusEnum.UNEMPLOYED
+    },
+    {
+      name: 'В поиске работы',
+      value: WorkStatusEnum.LOOKING
+    },
+  ];
+
+  sap = [
+    {
+      name: 'Необходимо записаться к кардиологу',
+      value: SymptomsOfAnginaPectorisEnum.GOTO_CARDIOLOGIST
+    },
+    {
+      name: 'Нет симптомов',
+      value: SymptomsOfAnginaPectorisEnum.NO_SYMPTOMS
+    },
+  ];
+
+  c = [
+    {
+      name: 'Низкий',
+      value: CommitmentEnum.LOW,
+    },
+    {
+      name: 'Средний',
+      value: CommitmentEnum.MEDIUM,
+    },
+    {
+      name: 'Высокий',
+      value: CommitmentEnum.HIGTH,
+    },
+  ];
+
+  v = [
+    'активная деятельная жизнь',
+    'жизненная мудрость',
+    'здоровье',
+    'интересная работа',
+    'красота природы и искусства',
+    'любовь',
+    'материально обеспеченная жизнь',
+    'наличие хороших и верных друзей',
+    'общественное призвание',
+    'познание и интеллектуальное развитие',
+    'продуктивная жизнь',
+    'развлечения',
+    'свобода, самостоятельность, независимость',
+    'счастливая семейная жизнь',
+    'творчество',
+    'уверенность в себе',
+  ]
 
 }
